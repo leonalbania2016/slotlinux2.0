b191ae9f2427aefbf4f2633baac1146954489e6d
leonalbania2016 <leonalbania2016@users.noreply.github.com>
2025-11-09 00:00:21 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,12 +22,12 @@ app.set('trust proxy', 1);
 
 
 app.use(
-cors({
-origin: process.env.FRONTEND_URL?.split(',') || ['http://localhost:5173'],
-credentials: true
-})
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  })
 );
-app.use(express.json({ limit: '2mb' }));
+
 
 
 app.use(
